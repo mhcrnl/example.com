54d671b18c30cb2928e8ce80a00b90dc3b60db7a
root <root@mhcrnl>
2014-11-15 18:33:19 +0200

    commit 15/11/2014

---
+++ public_html/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+ <form>
+	 <fieldset>
+		 <legend>Bucatarie Apa</legend>
+	Rece:<br>
+	<input type="text" name="firstname">
+	<br>
+	Calda:<br>
+	<input type="text" name="lastname">
+	</fieldset>
+</form>  
+
+</body>
+</html>
